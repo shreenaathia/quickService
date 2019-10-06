859c35a7129cc630c01d07e52dcfd6df88e16259
whatsupjs <you@example.com>
2019-10-06 17:12:53 -0400

    added footer

---
+++ quick_service/src/component/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        const contact = 'Contact QuickService \n Seneca@York Campus \n 70 The Pond Road \n Toronto, \n ON M3J 3M6';
+        return (
+            <div>
+                <footer id="footer">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-6">Copyright &copy; 2019 QuickSerivce</div>
+                            <div className="col-sm-6">
+                                <div className="newline">
+                                    {contact}
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
